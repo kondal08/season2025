31a80b8f7f22ab1a8368909936011f4a4d59313f
EXIBIST32132 <81558382+EXIBIST32132@users.noreply.github.com>
2025-01-09 23:02:27 +0000

    The Climber and new generic elevator class semi fixed, still need to fix every other subsytem

--- src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
@@ -2,48 +2,4 @@
 
 package frc.robot.subsystems.climber;
 
-public class ClimberIOSim implements ClimberIO {
-  @Override
-  public void updateInputs(ClimberIOInputs inputs) {}
-
-  @Override
-  public void setPosition(double climberPositionRad) {}
-
-  @Override
-  public void setVoltage(double volts) {}
-
-  @Override
-  public void setLeftVoltage(double volts) {}
-
-  @Override
-  public void setRightVoltage(double volts) {}
-
-  @Override
-  public void setHoming(boolean homingBool) {}
-
-  @Override
-  public void resetEncoder(double position) {}
-
-  @Override
-  public void resetEncoder() {
-    ClimberIO.super.resetEncoder();
-  }
-
-  @Override
-  public boolean isCurrentLimited() {
-    return ClimberIO.super.isCurrentLimited();
-  }
-
-  @Override
-  public boolean isLeftCurrentLimited() {
-    return ClimberIO.super.isLeftCurrentLimited();
-  }
-
-  @Override
-  public boolean isRightCurrentLimited() {
-    return ClimberIO.super.isRightCurrentLimited();
-  }
-
-  @Override
-  public void configurePID(double kP, double kI, double kD) {}
-}
+public class ClimberIOSim implements ClimberIO {}
