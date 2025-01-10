ee027419c2d88ee74b73f9e06dbb8b20bb7569db
EXIBIST32132 <81558382+EXIBIST32132@users.noreply.github.com>
2025-01-10 14:29:42 +0000

    Every single subsystem except for flywheel because might be useful for later, first description commit will commit as atomic commits

--- src/main/java/frc/robot/subsystems/pivot/PivotIO.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotIO.java
@@ -1,21 +1,5 @@
 package frc.robot.subsystems.pivot;
 
-import org.littletonrobotics.junction.AutoLog;
+import frc.robot.generic.elevators.GenericElevatorSystemIO;
 
-public interface PivotIO {
-  // let's only log the controller inputs for the leader
-  @AutoLog
-  public static class PivotIOInputs {
-    public double positionRad = 0.0;
-    public double absolutePositionRad = 0.0;
-    public double velocityRadPerSec = 0.0;
-    public double appliedVolts = 0.0;
-    public double currentAmps = 0.0;
-  }
-
-  /** Updates the set of loggable inputs. */
-  public void updateInputs(PivotIOInputs inputs);
-
-  /** Run open loop at the specified voltage. */
-  public void setVoltage(double volts);
-}
+public interface PivotIO extends GenericElevatorSystemIO {}
--- src/main/java/frc/robot/subsystems/pivot/PivotIOSim.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotIOSim.java
@@ -1,48 +1,12 @@
+// change between sim and sparkmax files
+
 package frc.robot.subsystems.pivot;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import frc.robot.generic.rollers.GenericRollerSystemIOSim;
 
-public class PivotIOSim implements PivotIO {
-
-  private SingleJointedArmSim sim =
-      new SingleJointedArmSim(
-          DCMotor.getNeoVortex(2),
-          (10 / Units.metersToInches(0.012) / 0.5),
-          1,
-          0.3126232,
-          0,
-          Units.degreesToRadians(110),
-          true,
-          0);
-
-  private PIDController pid = new PIDController(0.0, 0.0, 0.0);
-
-  private boolean closedLoop = false;
-  private double ffVolts = 0.0;
-  private double appliedVolts = 0.0;
-
-  @Override
-  public void updateInputs(PivotIOInputs inputs) {
-    if (closedLoop) {
-      appliedVolts = MathUtil.clamp(pid.calculate(sim.getAngleRads()) + ffVolts, -12.0, 12.0);
-      sim.setInputVoltage(appliedVolts);
+public class PivotIOSim extends GenericRollerSystemIOSim implements PivotIO {
+    public PivotIOSim(DCMotor motorModel, double reduction, double moi) {
+        super(motorModel, reduction, moi);
     }
-    sim.update(0.02);
-
-    inputs.positionRad = sim.getAngleRads();
-    inputs.velocityRadPerSec = sim.getVelocityRadPerSec();
-    inputs.appliedVolts = appliedVolts;
-    inputs.currentAmps = sim.getCurrentDrawAmps();
-  }
-
-  @Override
-  public void setVoltage(double volts) {
-    closedLoop = false;
-    appliedVolts = MathUtil.clamp(volts, -12.0, 12.0);
-    sim.setInputVoltage(volts);
-  }
 }
