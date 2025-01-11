10e82c4e76b19802bd20041a3495fcc316888e99
masterswordio <68434292+masterswordio@users.noreply.github.com>
2025-01-11 16:30:45 +0000

    Did the base code for Generic Elevator Systems

--- src/main/java/frc/robot/generic/elevators/GenericElevatorSystemIOSim.java
+++ src/main/java/frc/robot/generic/elevators/GenericElevatorSystemIOSim.java
@@ -1,3 +1,42 @@
 package frc.robot.generic.elevators;
 
-public class GenericElevatorSystemIOSim implements GenericElevatorSystemIO {}
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import frc.robot.generic.rollers.GenericRollerSystemIO;
+
+import static edu.wpi.first.math.system.plant.LinearSystemId.createDCMotorSystem;
+
+public class GenericElevatorSystemIOSim implements GenericElevatorSystemIO {
+    private final DCMotorSim sim;
+    private double appliedVoltage = 0.0;
+
+    public GenericElevatorSystemIOSim(DCMotor motorModel, double reduction, double moi) {
+        sim = new DCMotorSim(createDCMotorSystem(motorModel, reduction, moi), motorModel);
+    }
+
+
+    public void updateInputs(GenericElevatorSystemIO.GenericElevatorSystemIOInputs inputs) {
+        if (DriverStation.isDisabled()) {
+            runVolts(appliedVoltage);
+        }
+
+        sim.update(0.02);
+        inputs.positionMeters = sim.getAngularPositionRad();
+        inputs.velocityMetersPerSec = sim.getAngularVelocityRadPerSec();
+        inputs.appliedVoltage = appliedVoltage;
+        inputs.supplyCurrentAmps = sim.getCurrentDrawAmps();
+    }
+
+
+    public void runVolts(double volts) {
+        appliedVoltage = MathUtil.clamp(volts, -12.0, 12.0);
+        sim.setInputVoltage(appliedVoltage);
+    }
+
+    @Override
+    public void stop() {
+        runVolts(0.0);
+    }
+}
