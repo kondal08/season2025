c95e1693588cd0aa1eab795f6dc6ca3dd870d820
EXIBIST32132 <81558382+EXIBIST32132@users.noreply.github.com>
2025-01-11 16:56:28 +0000

    Fixing Rian's commit, small changes still amazing

--- src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
@@ -2,6 +2,11 @@
 
 package frc.robot.subsystems.climber;
 
+import edu.wpi.first.math.system.plant.DCMotor;
 import frc.robot.generic.elevators.GenericElevatorSystemIOSim;
 
-public class ClimberIOSim extends GenericElevatorSystemIOSim implements ClimberIO {}
+public class ClimberIOSim extends GenericElevatorSystemIOSim implements ClimberIO {
+    public ClimberIOSim(int numMotors, double startingAngle) {
+        super(numMotors, startingAngle);
+    }
+}
--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -1,8 +1,8 @@
 package frc.robot.subsystems.elevator;
 
 public final class ElevatorConstants {
-  public static final int LEFT_CLIMBER = 21; // TODO: Change to correct Motor ID's
-  public static final int RIGHT_CLIMBER = 22; // TODO: Change to correct Motor ID's
+  public static final int LEFT_ELEVATOR = 30; // TODO: Change to correct Motor ID's
+  public static final int RIGHT_ELEVATOR = 31; // TODO: Change to correct Motor ID's
 
   public static final double kP = 0.0003;
   public static final double kI = 0.0000008;
